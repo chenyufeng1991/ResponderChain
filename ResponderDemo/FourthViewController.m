76db9ba1ba2e02fe9480c730421fec13860502d2
chenyufeng <chenyufeng@chenyufengdeMacBook-Pro.local>
2016-05-13 14:28:47 +0800

    11

--- ResponderDemo/FourthViewController.m
+++ ResponderDemo/FourthViewController.m
@@ -31,14 +31,37 @@
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
+//获得整个触摸事件的响应链
 - (IBAction)getNextResponder:(id)sender
 {
     UIResponder *responder = sender;
-    NSLog(@"\n%@\n",responder);
+    NSLog(@"\n%@\n",[responder class]);
     while (responder != nil)
     {
         responder = [responder nextResponder];
-        NSLog(@"\n%@\n",responder);
+        NSLog(@"\n%@\n",[responder class]);
+    }
+}
+
+/**
+ * 在按钮点击后，在消息传递中当传到该VC对象以及UIApplication时，捕获该事件。
+ 一条链上的对象都可以获得响应；
+ */
+- (IBAction)captureEvent:(id)sender
+{
+    UIResponder *responder = sender;
+    while (responder != nil)
+    {
+        if ([responder isKindOfClass:[self class]])
+        {
+            NSLog(@"事件传递到该类VC,可以执行相应操作");
+        }
+
+        if ([responder isKindOfClass:[UIApplication class]])
+        {
+            NSLog(@"事件传递到UIApplication,可以执行相应操作");
+        }
+        responder = [responder nextResponder];
     }
 }
 
